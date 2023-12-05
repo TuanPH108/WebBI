e926ecd9a5ffe8b0cab14566982545708861985b
TuanPH108 <99632420+TuanPH108@users.noreply.github.com>
2023-12-06 04:35:31 +0700

    Update App.py
    remove

--- App.py
+++ App.py
@@ -1,10 +1,3 @@
-import streamlit as st
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from io import BytesIO
-import base64
 
 st.title('Real Estate Prediction')
 
